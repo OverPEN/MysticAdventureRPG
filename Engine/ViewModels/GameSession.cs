d0e41c19d56b76d66d9587fbd5326586ca3a47b8
Giuseppe Penna <penna_giuseppe@yahoo.it>
2020-12-13 02:35:41 +0100

    Aggiunta Location e prime images per Home

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -11,10 +11,18 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
-            CurrentPlayer = new Player("Giuseppe","Penna",PlayerClassType.Guerriero);
+            CurrentPlayer = new Player("Giuseppe","Penna",PlayerClassType.Mago);
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "Casa Dolce Casa";
+            CurrentLocation.ImageName = $"/Engine;component/Resources/LocationsImages/Home/Home_{CurrentPlayer.Class.ToString()}.jpg";
         }
     }
 }
