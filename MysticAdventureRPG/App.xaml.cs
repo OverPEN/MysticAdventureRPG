b9819df83dc85ef2560e69f7cbc7d42e07fe30f5
Giuseppe Penna <penna_giuseppe@yahoo.it>
2021-01-09 23:53:43 +0100

    Aggiunto ApplicationLogger

--- MysticAdventureRPG/App.xaml.cs
+++ MysticAdventureRPG/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ApplicationLogger;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace MysticAdventureRPG
 {
@@ -13,5 +15,15 @@ namespace MysticAdventureRPG
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"Errore durante l'esecuzione: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+
+            LoggingService.Log(e.Exception);
+
+            // TODO: Create a Window to display the exception information.
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
